18655a635f1eeb7fe24728ecf765e7ad80ab49f3
AKSHAY JAITLY <akshay365@icloud.com>
2017-10-21 20:16:30 -0400

    Update tableJS.js

--- js/tableJS.js
+++ js/tableJS.js
@@ -28,11 +28,11 @@ function toggle(source) {
 
 function myFunction2() {
   // Declare variables
-  var input_1, filter_1, table_1, tr_1, td_1, j;
-  input_1 = document.getElementById("myInput2");
-  filter_1 = input.value.toUpperCase();
-  table_1 = document.getElementById("myTable");
-  tr_1 = table.getElementsByTagName("tr");
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput2");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
