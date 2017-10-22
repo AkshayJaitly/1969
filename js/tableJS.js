5442aafe641e8fa6e48a2a1d424c58206e41b3fc
AKSHAY JAITLY <akshay365@icloud.com>
2017-10-21 20:14:22 -0400

    Update tableJS.js

--- js/tableJS.js
+++ js/tableJS.js
@@ -31,7 +31,7 @@ function myFunction2() {
   var input_1, filter_1, table_1, tr_1, td_1, j;
   input_1 = document.getElementById("myInput2");
   filter_1 = input.value.toUpperCase();
-  table_1 = document.getElementById("myTable2");
+  table_1 = document.getElementById("myTable");
   tr_1 = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
@@ -48,7 +48,7 @@ function myFunction2() {
 }
 
 function toggle(source) {
-  checkboxes = document.getElementsByName('bar');
+  checkboxes = document.getElementsByName('foo');
   for(var i=0, n=checkboxes.length;i<n;i++) {
     checkboxes[i].checked = source.checked;
   }
