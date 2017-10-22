04bade42da6836f8491564062daaf1eeb29874f5
AKSHAY JAITLY <akshay365@icloud.com>
2017-10-21 22:00:10 -0400

    Update tableJS.js

--- js/tableJS.js
+++ js/tableJS.js
@@ -28,17 +28,17 @@ function toggle(source) {
 
 function myFunction2() {
   // Declare variables
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
+  var input2, filter2, table, tr, td2, j;
+  input2 = document.getElementById("myInput2");
+  filter2 = input2.value.toUpperCase();
   table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
+  tr2 = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    td2 = tr[i].getElementsByTagName("td")[1];
+    if (td2) {
+      if (td2.innerHTML.toUpperCase().indexOf(filter2) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
@@ -48,7 +48,7 @@ function myFunction2() {
 }
 
 function toggle(source) {
-  checkboxes = document.getElementsByName('foo');
+  checkboxes = document.getElementsByName('bar');
   for(var i=0, n=checkboxes.length;i<n;i++) {
     checkboxes[i].checked = source.checked;
   }
